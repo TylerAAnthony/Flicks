5a9bd825e032ca98fdd66187842d0ed575655a3a
Tyler Anthony <Tyleranthony1906@gmail.com>
2016-01-29 14:27:25 -0600

    Finished table view

---
+++ movieViewer/MovieCell.swift
@@ -0,0 +1,35 @@
+//
+//  MovieCell.swift
+//  movieViewer
+//
+//  Created by Tyler Anthony on 1/29/16.
+//  Copyright © 2016 com.TylerAnthony. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    //Outlets
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var overviewLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+    @IBOutlet weak var starPic: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ movieViewer/movieViewer/MoviesViewController.swift
@@ -0,0 +1,122 @@
+//
+//  MoviesViewController.swift
+//  movieViewer
+//
+//  Created by Tyler Anthony on 1/29/16.
+//  Copyright © 2016 com.TylerAnthony. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+
+class MoviesViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
+{
+    
+/*******
+OUTLETS
+********/
+    //Table View Outlet
+    @IBOutlet weak var tableView: UITableView!
+    var movies : [NSDictionary]?
+
+    
+    
+    
+    
+/*
+View Did Load Function
+*/
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+
+        
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = NSURLRequest(
+            URL: url!,
+            cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData,
+            timeoutInterval: 10)
+        
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate: nil,
+            delegateQueue: NSOperationQueue.mainQueue()
+        )
+        
+        let task: NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            print("response: \(responseDictionary)")
+                            self.movies = responseDictionary["results"] as? [NSDictionary]
+                            self.tableView.reloadData()
+                            
+                            
+                    }
+                }
+        })
+        task.resume()
+
+        
+    }
+/*
+Did Receive Memory Warning Function
+*/
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+/*******
+Functions
+********/
+    
+    // NUMBER OF ROWS IN SECTION FUNCTION
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        if let movies = movies{
+            return movies.count
+        } else{
+            return 0
+        }
+        
+    
+    }
+    
+    // Cell for index path function
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell") as! MovieCell
+        let movie = movies![indexPath.row]
+        let title = movie["title"] as! String
+        let releaseDate = movie["release_date"] as! String
+        let overview = movie["overview"] as! String
+        let rating = movie["vote_average"] as! Float
+        let posterPath = movie["poster_path"] as! String
+        let base_url = "http://image.tmdb.org/t/p/w500"
+        let posterurl = NSURL(string: base_url + posterPath)
+        
+        
+        
+        
+        
+        cell.posterView.setImageWithURL(posterurl!)
+        cell.ratingLabel.text = "\(rating)/10"
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+    
+        
+        return cell
+        
+    }
+
+
+    
+
+   
+}
